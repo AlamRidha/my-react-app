13dec4033612c65256ae3a622b0a0cc86a4dffc7
AlamRidha <alamrridha@gmail.com>
2024-11-14 09:47:40 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div className="flex justify-center bg-blue-600 min-h-screen items-center text-stone-200">
+     <div>Hello World</div>
+    </div>
+  )
+}
+
+export default App
