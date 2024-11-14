4633017a8e1717a2cbe686c50afe2cff3526b185
AlamRidha <alamrridha@gmail.com>
2024-11-14 11:10:15 +0700

    Component and Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
-function App() {
+import React from "react";
+
+const Button = (props) => {
+  const { children = "....", variant = "bg-black" } = props;
 
   return (
-    <div className="flex justify-center bg-blue-600 min-h-screen items-center text-stone-200">
-     <div>Hello World</div>
+    <button
+      className={`h-10 px-6 font-semibold rounded-md ${variant} text-white`}
+      type="submit"
+    >
+      {children}
+    </button>
+  );
+};
+
+function App() {
+  return (
+    <div className="flex justify-center bg-blue-600 min-h-screen items-center">
+      <div className="flex gap-x-4">
+        <Button variant="bg-slate-900">Login</Button>
+        <Button variant="bg-red-700">Logout</Button>
+        <Button>Register</Button>
+        <Button></Button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
